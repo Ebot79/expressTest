327a0e6dff40be075e9aa97c78d685ed5d36280d
Ebot79 <eash79@gmail.com>
2013-10-08 03:59:19 +0000

    fixed jade files

--- index.js
+++ index.js
@@ -14,27 +14,31 @@ app.use(express.session({secret: 'my secret'}));
 //view options
 
 app.set('view engine', 'jade');
+app.use(app.router);
+//app.set('views', __dirname + '/views');
 
+//app.set('view options', {layout: false});
 
 //routes
 
 //default route
-app.get('/', function (req, res){
-
-	res.render('index', {authenticated: false})
-
+app.get('/', function (req, res, err){
+	console.log('default');
+	
+	res.render('index', {authenticated: false});
+	
 });
 
 //login route
 app.get('/login', function (req, res){
-
+	console.log('login');
 	res.render('login');
 
 });
 
 //signup route
 app.get('/signup', function(req, res){
-
+	console.log('signup');
 	res.render('signup');
 
 });
